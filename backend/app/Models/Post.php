2e4705a09be0998696f40a50cbb5ac8712dc55e7
taroosg <root@taroosg.dev>
2025-01-25 10:16:11 +0900

    idをulidに変更

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+  use HasUlids;
   protected $fillable = [
     'user_id',
     'content',
